297ead44a45cf4104535e3407056620a16dd41fc
Tobias Wieprich <tobiaswieprich@arcor.de>
2016-04-13 12:06:15 +0200

    Enable Profliling in mongodb

--- scripts/bsbm_generator/generateData.sh
+++ scripts/bsbm_generator/generateData.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd
 cd /bsbm/
-ls
-chmod +x generate
-./generate  -pc ${SCALING_FACTOR} -s sql
+sh generate  -pc ${SCALING_FACTOR} -s sql
 mv  dataset/* data/benchmark/sql
 ./generate  -pc ${SCALING_FACTOR} -s ttl
   mv dataset.ttl data/benchmark/ttl
--- scripts/mongodb/benchmark_mongodb.sh
+++ scripts/mongodb/benchmark_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd 
 cd /bsbm/scripts/mongodb/
-mongo -host ${MONGODB_PORT_27017_TCP_ADDR} -port ${MONGODB_PORT_27017_TCP_PORT} < query.js
+mongo -host ${MONGODB_PORT_27017_TCP_ADDR} -port ${MONGODB_PORT_27017_TCP_PORT} bsbm  < query.js
+
