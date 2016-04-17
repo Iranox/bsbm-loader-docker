0edcc230f0d084331f3b498cfba444ce3dc3fb61
Tobias Wieprich <tobiaswieprich@arcor.de>
2016-04-17 19:14:59 +0200

    Fix Error in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,18 +21,18 @@ sleep 3
 
 # loade data to mongo and materilize
 docker-compose run bsbmloader /bsbm/scripts/bsbmloader/mongoParser/parse_to_mongo.sh
-docker-compose run bsbmloader /bsbm/scripts/bsbmloader/mongoParser/materilize_n_to_1.sh
-docker-compose run bsbmloader /bsbm/scripts/bsbmloader/mongoParser/materilize_n_to_m.sh
+#docker-compose run bsbmloader /bsbm/scripts/bsbmloader/mongoParser/materilize_n_to_1.sh
+#docker-compose run bsbmloader /bsbm/scripts/bsbmloader/mongoParser/materilize_n_to_m.sh
 
 
 
 #import data to elasticsearch
-docker-compose run bsbmloader  sh /bsbm/scripts/bsbmloader/elasticParser/parse_to_elastic.sh
+#docker-compose run bsbmloader  sh /bsbm/scripts/bsbmloader/elasticParser/parse_to_elastic.sh
 
 # import data to couchdb
- docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/parse_to_couch.sh
- docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/materilize_n_to_1.sh
- docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/materilize_n_to_m.sh
+# docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/parse_to_couch.sh
+# docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/materilize_n_to_1.sh
+# docker-compose run bsbmloader /bsbm/scripts/bsbmloader/couchParser/materilize_n_to_m.sh
  #sleep 3
 
 # import data to postgres
