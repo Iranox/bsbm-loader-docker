e91aab65a0749c924dee35524c153560cf36d0b9
Jörg Unbehauen <joerg@unbehauen.net>
2016-05-08 18:38:01 +0200

    retain td_data

--- scripts/bsbm_generator/generateData.sh
+++ scripts/bsbm_generator/generateData.sh
@@ -3,5 +3,7 @@ cd
 cd /bsbm/
  ./generate  -pc ${SCALING_FACTOR} -s sql
 mv  dataset/* data/benchmark/sql
+mv td_data data/benchmark/sql
 ./generate  -pc ${SCALING_FACTOR} -s ttl
 mv dataset.ttl data/benchmark/ttl
+mv td_data data/benchmark/sql
