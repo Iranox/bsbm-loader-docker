97b7876a02e9bdf317e61dc265a764ca2dc63a06
Tobias Wieprich <tobiaswieprich@arcor.de>
2016-05-26 20:44:21 +0200

    Fix an error

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,19 +2,13 @@
 
 cd ..
 
-#start docker compose
-#docker-compose up &
-if [[ "$generateData" == "\"true\"" ]]
-  then
-      #generate sql and ttl files
-      docker-compose run bsbm_generator sh /bsbm/scripts/bsbm_generator/generateData.sh
 
-      # import data to mysql
-      docker-compose run mysql sh /bsbm/scripts/mysql/import_sql.sh
-      sleep 10
-fi
+#generate sql and ttl files
+docker-compose run bsbm_generator sh /bsbm/scripts/bsbm_generator/generateData.sh
 
+# import data to mysql
+docker-compose run mysql sh /bsbm/scripts/mysql/import_sql.sh
+sleep 10
 
-# loade data to nosql
-docker-compose run bsbmloader sh /bsbm/scripts/bsbmloader/parse_to_nosql.sh
-
+# load data to nosql
+docker-compose run bsbmloader bash /bsbm/scripts/bsbmloader/parse_to_nosql.sh
