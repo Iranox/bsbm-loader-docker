471d1061ccffe85b3b94df4f3a44ded1582db2a5
Iranox <aasgar@arcor.de>
2016-05-31 16:08:19 +0200

    This closed #14 .

--- scripts/mongodb/queries_mat/query1_mat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mongo -host ${MONGODB_PORT_27017_TCP_ADDR} -port ${MONGODB_PORT_27017_TCP_PORT}  bsbm --eval 'db.offer.aggregate([{$match: {nr:50}},{ $project: {_id:0,product:1, producer:1,vendor:1, price:1, validFrom:1, validTo:1, deliveryDays:1, offerWebpage:1, publisher:1, publishDate:1}}]).pretty()'
-
-mongo -host ${MONGODB_PORT_27017_TCP_ADDR} -port ${MONGODB_PORT_27017_TCP_PORT}  bsbm --eval 'db.offer.aggregate([{$match: {nr:50}},{ $project: {_id:0,product:1, producer:1,vendor:1, price:1, validFrom:1, validTo:1, deliveryDays:1, offerWebpage:1, publisher:1, publishDate:1}}]).pretty().explain("executionStats")'
--- scripts/mongodb/queries_notmat/query2_notmat.js
+++
@@ -1,5 +0,0 @@
-use bsbm;
-db.setProfilingLevel(2)
-db.getCollectionNames()
-db.product.aggregate([ {$match: {nr:20}}, {$lookup: {from: "producer", localField: "producer", foreignField: "nr", as:"pr" }}, {$lookup: {  from: "productfeatureproduct", localField: "nr",foreignField: "product",as:"pfp" }},{ $project: {_id:0, propertyTex1:1, propertyTex2:1, propertyTex3:1,propertyNum1:1, propertyNum2:1, propertyTex4:1, propertyTex5:1, propertyNum4:1,"pfp.productFeature":1,comment:1,"pr.nr":1}}])
-db.system.profile.find().pretty()
