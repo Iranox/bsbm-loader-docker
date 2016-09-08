4a1d8bda129dcc6c862520d19a3bad4203e009e9
Iranox <aasgar@arcor.de>
2016-09-08 17:09:56 +0200

    Add teiid files.

---
+++ teiid-docker/scripts/configTeiid.sh
@@ -0,0 +1,4 @@
+cd ..
+docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com
+docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com/mysql
+docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com/mysql/main
