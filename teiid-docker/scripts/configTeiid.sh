6a9420eb5138af9ead47bf27f6aa8166ec3bb95c
Tobias Wieprich <tobiaswieprich@arcor.de>
2016-10-06 15:28:20 +0200

    remove teiid and teiid-docker folder

--- teiid-docker/scripts/configTeiid.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com
-docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com/mysql
-docker-compose run teiid mkdir teiid-8.11.0.Final/modules/com/mysql/main
--- teiid/installTeiid.sh
+++
@@ -1,8 +0,0 @@
-wget https://repository.jboss.org/nexus/service/local/repositories/releases/content/org/jboss/teiid/teiid/8.13.6/teiid-8.13.6-wildfly-server.zip
-unzip teiid-8.13.6-wildfly-server.zip
-mv teiid-8.13.6 /opt
-mv /com /opt/teiid-8.13.6/modules
-rm /opt/teiid-8.13.6/configuration/standalone-teiid.xml
-mv /standalone/configuration/standalone-teiid.xml /opt/teiid-8.13.6/configuration/standalone-teiid.xml
-mv /standalone/deployment/mysql-vdb.xml /opt/teiid-8.13.6/standalone/deployment/mysql-vdb.xml
-
