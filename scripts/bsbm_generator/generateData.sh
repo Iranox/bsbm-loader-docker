f4ccc60ad3a288e081244a0849ae12d9c87afa63
Tobias Wieprich <tobiaswieprich@arcor.de>
2016-10-06 16:21:47 +0200

    remove scripts

--- scripts/bsbm_generator/generateData.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd
-cd /bsbm/
- ./generate  -pc ${SCALING_FACTOR} -s sql
-mv  dataset/* data/benchmark/sql
-mv -f td_data data/benchmark/sql
-./generate  -pc ${SCALING_FACTOR} -s ttl
-mv dataset.ttl data/benchmark/ttl
-mv -f td_data data/benchmark/ttl
--- scripts/mongodb/create_product_feature_typ.js
+++
@@ -1,1 +0,0 @@
-db.product_matComplex.aggregate([{$lookup: { from: "producttypeproduct", localField: "nr",foreignField: "product",as: "type"}},{$out:"product_feature_typ"}])
