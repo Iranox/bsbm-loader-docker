452d4b041d9eb443c09f33c4f83e7db728a5449e
Iranox <aasgar@arcor.de>
2016-11-07 21:38:28 +0100

    Remove unneeded scripts. #23

--- scenario/base/scripts/mysql/test/testMySql2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ret=$(mysql -u root --password=$MYSQL_VERTICAL_MYSQL2_1_ENV_MYSQL_ROOT_PASSWORD  -s -h $MYSQL_VERTICAL_MYSQL2_1_PORT_3306_TCP_ADDR -P $MYSQL_VERTICAL_MYSQL2_1_PORT_3306_TCP_PORT   -e "Select count(*) from person" benchmark 2>/dev/null)
-echo $ret
--- scenario/mysql_vertical/startTest.sh
+++ scenario/mysql_vertical/startTest.sh
@@ -3,13 +3,19 @@
 #import data
 sh start.sh 100
 
+# set environments
+mysql1Host=$(docker-compose run mysql_vertical_mysql1 printenv MYSQL_VERTICAL_MYSQL1_PORT_3306_TCP_ADDR)
+mysql2Host=$(docker-compose run mysql_vertical_mysql2 printenv MYSQL_VERTICAL_MYSQL2_PORT_3306_TCP_ADDR)
+
 #test First database
-ret=$(docker-compose run mysql_vertical_mysql1 sh /bsbm/scripts/mysql/test/testMySql1.sh)
-ret2=$(docker-compose run mysql_vertical_mysql2 sh /bsbm/scripts/mysql/test/testMySql2.sh)
+ret=$(docker-compose run mysql_vertical_mysql1 mysql -u root --password=password  -s -h  $(echo $mysql1Host) -P  3306   -e "Select count(*) from product" benchmark 2>/dev/null)
+ret2=$(docker-compose run mysql_vertical_mysql2 mysql -u root --password=password  -s -h  $(echo $mysql2Host) -P  3306   -e "Select count(*) from person" benchmark 2>/dev/null)
 
-echo $ret
-echo $ret2
+# remove unneeded literal
+ret=$(echo $ret | tr -d -c 0-9)
+ret2=$(echo $ret2 | tr -d -c 0-9)
 
+# Check if database product contains 100 rows
 if [ $ret -eq 100 ]
   then
      echo "All 100 products were inserted"
@@ -17,6 +23,7 @@ if [ $ret -eq 100 ]
      exit 1
 fi
 
+# Check if person product contains 50 rows
 if [ $ret2 -eq 50 ]
   then
      echo "All 50 persons were inserted"
@@ -24,4 +31,5 @@ if [ $ret2 -eq 50 ]
      exit 1
 fi
 
+#stop databases
 docker-compose stop
