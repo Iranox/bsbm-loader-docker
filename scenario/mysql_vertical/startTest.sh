0131a15c1d27ea9fee3feca1d00e98f816019101
Iranox <aasgar@arcor.de>
2016-11-29 00:25:54 +0100

    Fix Build

--- scenario/mysql_vertical/startTest.sh
+++ scenario/mysql_vertical/startTest.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-cd ../../scenario/mysql_vertical/
-#import data
-sh start.sh 100
+
+#generate sql and ttl files
+
+docker-compose -f ../base/docker-compose.yml -f docker-compose.yml up -d
+sleep 60
+
+docker-compose -f ../base/docker-compose.yml -f docker-compose.yml \
+      run base_bsbm_generator sh /bsbm/scripts/bsbm_generator/generateData.sh 100
+
+sleep 60
+
+docker-compose  -f ../base/docker-compose.yml -f docker-compose.yml run \
+              base_mysql sh /bsbm/scripts/mysql/importMysql1.sh
+
+
+docker-compose -f ../base/docker-compose.yml -f docker-compose.yml \
+               run mysql_vertical_mysql2 sh /bsbm/scripts/mysql/importMysql2.sh
 
 
 #test First database
