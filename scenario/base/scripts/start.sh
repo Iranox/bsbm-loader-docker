74f25feee3ad085ab2f629ed09e9645d776d195e
Iranox <aasgar@arcor.de>
2016-12-31 16:35:07 +0100

    Fix start.sh Close #26

--- scenario/base/scripts/start.sh
+++ scenario/base/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-
+docker-compose up -d
 
 #generate sql and ttl files
 docker-compose run base_bsbm_generator sh /bsbm/scripts/bsbm_generator/generateData.sh $1
@@ -9,5 +9,3 @@ docker-compose run base_bsbm_generator sh /bsbm/scripts/bsbm_generator/generateD
 # import data to mysql
 docker-compose run base_mysql sh /bsbm/scripts/mysql/import_sql.sh
 sleep 10
-
-docker-compose run base_bsbmloader bash /bsbm/scripts/bsbmloader/parse_to_nosql.sh $2
